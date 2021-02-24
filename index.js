fbd2fd8e1e9757dd1433b0456f78f23fe6abf6b9
Nahum <losond8@gmail.com>
2021-02-24 13:28:15 -0800

    Slight hangup on this lab regarding the global scope aspect. Need to get better with reading the test scripts and fully understanding what they are asking

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = 'bob'.toUpperCase()
+}
+
+function setBestCustomer(){
+ bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+ }
+
+ const leastFavoriteCustomer = "higher order functions"
+
+ function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = "inconsistent test scripts"
+ }
+
+//  leastFavoriteCustomer and changeLeastFavoriteCustomer():
+//   Now declare a constant (using the const keyword) in global 
+//   scope called leastFavoriteCustomer; be sure to assign it 
+//   some initial value. Finally, write a function called changeLeastFavoriteCustomer() that attempts to
+//   change that constant - notice what JavaScript does when you try to change the constant.
